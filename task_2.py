76b30f625cc7de88a0c40165c5eae7dd5a88fa55
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-10-19 09:59:57 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,7 @@
-def chess_board(n, k):
-    pass
+def chess_board(n,k):
+    for p in range(0,2 * n):
+        for i in range(0,2):
+            print((" " * k + "#" * k ) * n)
+        for j in range (2,4):
+            print (("#" * k + " " * k ) * n)
+chess_board(4,2)
