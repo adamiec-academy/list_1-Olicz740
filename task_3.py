a099cb50fbbaae3362175161f9a1bd7d9f87b556
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-10-23 18:16:58 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,16 @@
 def envelope(n):
-    pass
+    p1 = 0
+    p2 = n - 4
+    print(9 * "*")
+    for _ in range((n // 2) -1):
+        print("*", " " * p1, "*", " " * p2, "*", " " * p1, "*", sep="")
+        p1 = p1 + 1
+        p2 = p2 - 2
+    print("*", " " * (p1 - 2), "*", " " * (p1 - 2), "*")
+    for _ in range((n // 2) - 1):
+        print("*", " " * (p1 - 1), "*", " " * (p2 + 2), "*", " " * (p1 - 1), "*", sep="")
+        p1 = p1 - 1
+        p2 = p2 + 2
+    print(9 * "*")
+
+envelope(9)
