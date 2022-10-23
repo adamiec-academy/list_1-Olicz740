fa8f1ae6a276737ac66e7fce477ebffd98c58c37
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-10-23 18:15:50 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,6 +1,19 @@
 def snowball(n, k):
-    pass
+    m =  0.5
+    r = n/2
 
+    for i in range(n):
+        print( k * " ", end = "")
+        for j in range(n):
+            if (i + m - r) ** 2 + (j + m -r) ** 2 <= r ** 2:
+                print("X", end="")
+            else:
+                print(" ", end="")
+        print()
 
 def snowman(n):
-    pass
+    snowball(n, 2)
+    snowball(n + 2, 1)
+    snowball(n + 4, 0)
+    print()
+snowman(2)
