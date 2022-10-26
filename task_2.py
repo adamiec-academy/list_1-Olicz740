4792fe4d73475b9e91cc884cf98bf4c0955cfbd5
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-10-26 10:15:35 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -2,6 +2,6 @@ def chess_board(n,k):
     for _ in range(n):
         for _ in range(k):
             print((" " * k + "#" * k ) * n)
-        for _ in range (k, k + 2):
+        for _ in range (k):
             print (("#" * k + " " * k ) * n)
 chess_board(4,2)
